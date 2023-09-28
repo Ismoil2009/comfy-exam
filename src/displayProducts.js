9c573f0c2a19df25d8e8f67a38246bb104021372
Ismoil2009 <osnovavbs092009@gmail.com>
2023-09-28 22:21:45 +0500

    finish

---
+++ src/displayProducts.js
@@ -0,0 +1,36 @@
+import { formatPrice } from "./utils.js";
+
+const display = (products, element, filters) => {
+  element.innerHTML = products
+    .map((product) => {
+      const { id, name, image, price } = product;
+
+      return `<article class="product">
+          <div class="product-container">
+            <img src="${image}" class="product-img img" alt="${name}" />
+            <div class="product-icons">
+              <a href="product.html?id=${id}" class="product-icon">
+                <i class="fas fa-search"></i>
+              </a>
+            </div>
+          </div>
+          <footer>
+            <p class="product-name">${name}</p>
+            <h4 class="prouct-price">${formatPrice(price)}</h4>
+          </footer>
+        </article>`;
+    })
+    .join("");
+
+  if (filters) return;
+
+  element.addEventListener("click", function (e) {
+    const parent = e.target.parentElement;
+
+    if (parent.classList.contains("product-cart-btn")) {
+      addToCart(parent.dataset.id);
+    }
+  });
+};
+
+export default display;
