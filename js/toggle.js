5173c468e7370991ac9214e3acc20c7192617325
Ismoil2009 <osnovavbs092009@gmail.com>
2023-09-29 07:34:16 +0500

    fixing bugs

--- js/toggle.js
+++ js/toggle.js
@@ -1,13 +1,1 @@
 import { getElement } from "./utils.js";
-
-const toggleNav = getElement(".toggle-nav");
-const sideBarOverlay = getElement(".sidebar-overlay");
-const closeBtn = getElement(".sidebar-close");
-
-toggleNav.addEventListener("click", () => {
-  sideBarOverlay.classList.add("show");
-});
-
-closeBtn.addEventListener("click", () => {
-  sideBarOverlay.classList.remove("show");
-});
